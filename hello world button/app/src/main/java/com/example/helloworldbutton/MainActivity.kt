c1840b84e63b62d3f8c83eae8ff473766b18548b
Jingbo <wangjb12018@gmail.com>
2024-09-06 15:42:31 -0400

    upload IA Q1 and Q2

---
+++ hello world button/app/src/main/java/com/example/helloworldbutton/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.helloworldbutton
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworldbutton.ui.theme.HelloWorldButtonTheme
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MyButton()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun MyButton() {
+    // Initial value is null
+    var name by remember { mutableStateOf<String?>(null) }
+
+    // If name is null, display nothing. if not, display name
+    name?.let {
+        Greeting(name = it)
+    }
+
+    Button(onClick = {
+        name = "World"
+    },
+        modifier = Modifier.padding(16.dp)) {
+        Text(text = "Click Me")
+    }
+}
